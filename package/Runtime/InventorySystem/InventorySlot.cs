88bbde722a5ed38170a606cb5eeb0815c6bc339f
이정훈 <eu4ng97@gmail.com>
2025-04-01 01:25:59 +0900

    Feat: InventorySlot 구조체 작성

---
+++ package/Runtime/InventorySystem/InventorySlot.cs
@@ -0,0 +1,16 @@
+using System;
+using Eu4ng.System.Item;
+using UnityEngine;
+
+namespace Eu4ng.System.Inventory
+{
+    [Serializable]
+    public struct InventorySlot
+    {
+        public int Index;
+
+        public ItemDefinition Definition;
+
+        public int Quantity;
+    }
+}
