99c5244afa9588856da44dbbe15558dd2ea50548
이정훈 <eu4ng97@gmail.com>
2025-04-01 01:30:20 +0900

    Feat: ItemConfig_Inventory 구조체 작성

---
+++ package/Runtime/InventorySystem/ItemConfig_Inventory.cs
@@ -0,0 +1,21 @@
+using System;
+using Eu4ng.System.Item;
+using UnityEngine;
+
+namespace Eu4ng.System.Inventory
+{
+    [Serializable]
+    public struct InventoryItemConfigData
+    {
+        [Min(0)]
+        public int MaxStack;
+    }
+
+#if USE_E4_INVENTORY_SYSTEM
+    [CreateAssetMenu(fileName = "ItemConfig_Inventory", menuName = "Scriptable Objects/ItemConfig/Inventory")]
+#endif
+    public class ItemConfig_Inventory : ItemConfig<InventoryItemConfigData>
+    {
+
+    }
+}
