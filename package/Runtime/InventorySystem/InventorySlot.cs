55823fedbe9375eb8c84e88cac91e3e7d6d507f9
이정훈 <eu4ng97@gmail.com>
2025-04-01 02:06:42 +0900

    Feat: InventorySlot 구조체에 IEquatable 인터페이스 구현

--- package/Runtime/InventorySystem/InventorySlot.cs
+++ package/Runtime/InventorySystem/InventorySlot.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Eu4ng.System.Inventory
 {
     [Serializable]
-    public struct InventorySlot
+    public struct InventorySlot : IEquatable<InventorySlot>
     {
         public int Index;
 
@@ -15,5 +15,20 @@ namespace Eu4ng.System.Inventory
 
         public static bool operator ==(InventorySlot inventorySlot, int index) => inventorySlot.Index == index;
         public static bool operator !=(InventorySlot inventorySlot, int index) => !(inventorySlot == index);
+
+        public bool Equals(InventorySlot other)
+        {
+            return Index == other.Index && Equals(Definition, other.Definition) && Quantity == other.Quantity;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is InventorySlot other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Index, Definition, Quantity);
+        }
     }
 }
